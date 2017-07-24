c4f02129c6941fc97a288a594ed76212474733c7
kch <kch3338@naver.com>
2017-07-24 16:47:10 +0900

    최초 커밋

---
+++ src/main/java/com/hanbit/there/api/KchBackApplication.java
@@ -0,0 +1,12 @@
+package com.hanbit.there.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KchBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KchBackApplication.class, args);
+	}
+}
